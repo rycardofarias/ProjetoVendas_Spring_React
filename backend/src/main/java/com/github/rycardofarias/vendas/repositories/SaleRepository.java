7708656026172ae045c933c11042ec8f8261fe94
Ricardo <ricardofariasxyz@gmail.com>
2021-05-08 01:56:54 -0300

    Group by search

--- backend/src/main/java/com/github/rycardofarias/vendas/repositories/SaleRepository.java
+++ backend/src/main/java/com/github/rycardofarias/vendas/repositories/SaleRepository.java
@@ -1,7 +1,21 @@
 package com.github.rycardofarias.vendas.repositories;
 
 import com.github.rycardofarias.vendas.entities.Sale;
+import com.github.rycardofarias.vendas.entities.SaleSuccessDTO;
+import com.github.rycardofarias.vendas.entities.SaleSumDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+
+    @Query("SELECT new com.github.rycardofarias.vendas.entities.SaleSumDTO(obj.seller, SUM(obj.amount)) "
+            + "FROM Sale AS obj GROUP BY obj.seller")
+    List<SaleSumDTO> amountGroupeBySeller();
+
+    @Query("SELECT new com.github.rycardofarias.vendas.entities.SaleSuccessDTO(obj.seller, SUM(obj.visited), SUM(obj.deals)) "
+            + "FROM Sale AS obj GROUP BY obj.seller")
+    List<SaleSuccessDTO> successGroupeBySeller();
 }
--- backend/src/main/java/com/github/rycardofarias/vendas/service/SaleService.java
+++ backend/src/main/java/com/github/rycardofarias/vendas/service/SaleService.java
@@ -2,6 +2,8 @@ package com.github.rycardofarias.vendas.service;
 
 import com.github.rycardofarias.vendas.dto.SaleDTO;
 import com.github.rycardofarias.vendas.entities.Sale;
+import com.github.rycardofarias.vendas.entities.SaleSuccessDTO;
+import com.github.rycardofarias.vendas.entities.SaleSumDTO;
 import com.github.rycardofarias.vendas.repositories.SaleRepository;
 import com.github.rycardofarias.vendas.repositories.SellerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class SaleService {
@@ -24,5 +28,14 @@ public class SaleService {
         sellerRepository.findAll();
         Page<Sale> result = repository.findAll(pageable);
         return result.map(x -> new SaleDTO(x));
+    }
+    @Transactional(readOnly = true)
+    public List<SaleSumDTO> amountGroupedBySeller(){
+        return repository.amountGroupeBySeller();
+    }
+
+    @Transactional(readOnly = true)
+    public List<SaleSuccessDTO> successGroupedBySeller(){
+        return repository.successGroupeBySeller();
     }
 }
