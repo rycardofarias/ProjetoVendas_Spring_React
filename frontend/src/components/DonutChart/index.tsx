f810425fbca5767f459ae41cbb45edd497b2bf1f
Ricardo <ricardofariasxyz@gmail.com>
2021-05-08 20:50:15 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,43 +1,46 @@
 import axios from 'axios';
 import { SaleSum } from 'components/types/sale';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts'
 import { BASE_URL } from 'utils/requests';
 
 type ChartData = {
     labels: string[];
-    series: number [];
+    series: number[];
 }
 
 const DonutChart = () => {
 
-    //FORMA ERRADA    
-    let chartData : ChartData = { labels: [], series: []};
-    
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-        .then( (response) => {
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then((response) => {
 
-            const data = response.data as SaleSum[];
-            const myLabels = data.map(x => x.sellerName);
-            const mySeries = data.map(x => x.sum);
-            
-            chartData = { labels: myLabels, series: mySeries}
-            console.log(chartData);
-        })
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
+
+                setChartData({ labels: myLabels, series: mySeries })
+                console.log(chartData);
+            })
+    }, [] );
+
+
     //const mockData = {
-        //series: [477138, 499928, 444867, 220426, 473088],
-        //labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+    //series: [477138, 499928, 444867, 220426, 473088],
+    //labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     //}
-    
+
     const options = {
         legend: {
             show: true
         }
     }
-    
-    return(
-        <Chart 
-            options={{ ...options, labels: chartData.labels}}
+
+    return (
+        <Chart
+            options={{ ...options, labels: chartData.labels }}
             series={chartData.series}
             type="donut"
             height="240"
--- frontend/src/components/types/sale.ts
+++ frontend/src/components/types/sale.ts
@@ -3,3 +3,7 @@ export type SaleSum = {
     sum: number
 }
 
+export const round = (value: number, precision: number) => {
+    var multiplier = Math.pow(10, precision || 0);
+    return Math.round(value * multiplier) / multiplier;
+}
