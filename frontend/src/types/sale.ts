3b7bb2c0ab405b17308ad8d1e4f7c0470573e7e6
Ricardo <ricardofariasxyz@gmail.com>
2021-05-08 23:02:31 -0300

    Move

---
+++ frontend/src/types/sale.ts
@@ -0,0 +1,37 @@
+import { Seller } from './seller';
+
+export type Sale = {
+    id : number,
+    visited : number,
+    deals : number,
+    amount: number,
+    date : string;
+    seller : Seller;
+}
+
+export type SalePage = {
+    content?: Sale[];
+    last : boolean,
+    totalPages : number,
+    number : number,
+    size ? : number;
+    first: boolean,
+    numberOfElements ? : number,
+    empty ? : boolean
+}
+
+export type SaleSum = {
+    sellerName: string,
+    sum: number
+}
+
+export type SaleSuccess = {
+    sellerName: string;
+    visited: number;
+    deals: number;
+}
+
+export const round = (value: number, precision: number) => {
+    var multiplier = Math.pow(10, precision || 0);
+    return Math.round(value * multiplier) / multiplier;
+}
