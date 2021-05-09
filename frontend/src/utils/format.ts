aeb886e0aa9405004805802fe85df2ec3b0fccb2
Ricardo <ricardofariasxyz@gmail.com>
2021-05-08 21:20:51 -0300

    BarChart integration

---
+++ frontend/src/utils/format.ts
@@ -0,0 +1,10 @@
+export const round = (value: number, precision: number) => {
+    var multiplier = Math.pow(10, precision || 0);
+    return Math.round(value * multiplier) / multiplier;
+}
+
+export type SaleSuccess = {
+    sellerName: string;
+    visited: number;
+    deals: number;
+}
